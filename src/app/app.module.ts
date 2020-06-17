bbaa016aa25a337392b024c820ac92b9924bb0a8
luongquy <quylv@itsol.vn>
2020-06-17 22:11:29 +0700

    slick

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { SlickComponent } from './slick/slick.component';
+import { TestSlickComponent } from './test-slick/test-slick.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    SlickComponent,
+    TestSlickComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
